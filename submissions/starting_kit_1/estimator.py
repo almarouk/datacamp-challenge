28624428998b37752708113feded6d4342e4a9eb
almarouk <72821992+almarouk@users.noreply.github.com>
2021-01-18 00:53:44 +0100

    Add files via upload

---
+++ submissions/starting_kit_1/estimator.py
@@ -0,0 +1,23 @@
+from sentence_transformers import SentenceTransformer
+from sklearn.linear_model import LogisticRegression
+import pandas
+
+class CustomClassifier(BaseEstimator):
+    """Dummy detector which ouputs 1-second long steps every two seconds."""
+
+    def __init__(self):
+    	self.model = SentenceTransformer('distilbert-base-nli-mean-tokens')
+    	self.clf = LogisticRegression(random_state=42)
+
+    def fit(self, X, y):
+        embeddings = self.model.encode(list(X['Text']), show_progress_bar=True)
+        self.clf.fit(embeddings, y)
+        return self
+
+    def predict(self, X):
+        return self.clf.predict(self.model.encode(list(X['Text'])))
+
+
+def get_estimator():
+
+    return CustomClassifier()
