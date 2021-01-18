36ef451594c245cccbae4998103be6e50046428d
SabaKiriako <67561819+SabaKiriako@users.noreply.github.com>
2021-01-18 02:27:46 +0100

    Add files via upload

--- submissions/starting_kit_1/estimator.py
+++ submissions/starting_kit_1/estimator.py
@@ -1,6 +1,8 @@
 from sentence_transformers import SentenceTransformer
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.base import BaseEstimator
+import numpy as np
 import pandas
 
 class CustomClassifier(BaseEstimator):
@@ -12,13 +14,15 @@ class CustomClassifier(BaseEstimator):
 
     def fit(self, X, y):
         embeddings = self.model.encode(list(X['Text']), show_progress_bar=True)
+        print(y.shape)
         self.clf.fit(embeddings, y)
         return self
 
     def predict(self, X):
-        return self.clf.predict(self.model.encode(list(X['Text'])))
+        predict = self.clf.predict(self.model.encode(list(X['Text'])))
+        enc=OneHotEncoder()
+        return enc.fit_transform(predict.reshape(-1,1)).toarray()
 
 
 def get_estimator():
-
     return CustomClassifier()
